b9a417c47d96fbd52e5ea8fbe40436198a51b577
Axel-Cleris Gailloty <mr.clerisaxel@hotmail.fr>
2022-11-17 11:16:49 +0100

    Ajoutez des fichiers projet.

---
+++ CleverCSV/TypeGuesser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace CleverCSV
+{
+    /// <summary>
+    /// A class that guesses the type of a primitive value in a string
+    /// </summary>
+    public class TypeGuesser
+    {
+        private string _value;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        public TypeGuesser(string value)
+        {
+            _value = value;
+        }
+
+        /// <summary>
+        /// Returns the type of .NET primitive. 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public Type GuessValueType()
+        {
+            bool isSuccess = false;
+            bool isBool;
+            isSuccess = bool.TryParse(_value, out isBool);
+
+            if (isSuccess)
+                return typeof(bool);
+
+            int isInt;
+            isSuccess = int.TryParse(_value, out isInt);
+
+            if (isSuccess)
+                return typeof(int);
+
+            double isDouble;
+            isSuccess = double.TryParse(_value, out isDouble);
+            if (isSuccess)
+                return typeof(double);
+
+            DateTime isDate;
+
+            isSuccess = DateTime.TryParse(_value, out isDate);
+            if (isSuccess)
+                return typeof(DateTime);
+
+
+            return typeof(string);
+        }
+    }
+}
---
+++ TestCleverCSV/Program.cs
@@ -0,0 +1,19 @@
+﻿using CleverCSV;
+namespace TestCleverCSV
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            TypeGuesser _guesser;
+            string[] data = { "age", "20", "20.5", "22.55", "23/08/2022", "2022-08-12", "2022/06/11" };
+
+            foreach (string s in data)
+            {
+                _guesser = new TypeGuesser(s);
+                Console.WriteLine($"The guessed type of {s} is {_guesser.GuessValueType().Name}");
+            }
+        }
+    }
+}
