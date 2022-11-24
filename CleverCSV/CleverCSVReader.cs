7da0bdededb4ca49c0da6fadc29bbc874f5fb46e
Axel-Cleris Gailloty <mr.clerisaxel@hotmail.fr>
2022-11-24 14:55:20 +0100

    Adding main CleverCSVReader class

---
+++ CleverCSV/CleverCSVReader.cs
@@ -0,0 +1,25 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace CleverCSVReader
+{
+    public class CleverCSVReader : CsvReader
+    {
+        public CleverCSVReader(IParser parser) : base(parser)
+        {
+        }
+
+        public CleverCSVReader(TextReader reader, CultureInfo culture, bool leaveOpen = false) : base(reader, culture, leaveOpen)
+        {
+        }
+
+        public CleverCSVReader(TextReader reader, IReaderConfiguration configuration, bool leaveOpen = false) : base(reader, configuration, leaveOpen)
+        {
+        }
+    }
+}
