b8d512184c563bc714660c0cd58e05c3d305e700
Axel-Cleris Gailloty <mr.clerisaxel@hotmail.fr>
2022-12-03 20:26:06 +0100

    Add a project to generate a class based on the guessed types

---
+++ ObjectGenerator/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+
+namespace ObjectGenerator
+{
+    public class ObjectTypeMapper
+    {
+        private readonly Type[] types;
+        private readonly string[] propertyNames;
+        private readonly string className;
+
+        public ObjectTypeMapper(Type[] types, string[] propertyNames, string className)
+        {
+            this.types = types;
+            this.propertyNames = propertyNames;
+            this.className = className;
+        }
+
+        public void GenerateType(IDictionary<string, Type> publicProperties)
+        {
+            CodeCompileUnit compileUnit = new CodeCompileUnit();
+            var exampleNamespace = new CodeNamespace("ExampleNamespace");
+            exampleNamespace.Imports.Add(new CodeNamespaceImport("System"));
+            compileUnit.Namespaces.Add(exampleNamespace);
+
+            var codeMemberProperty = new CodeMemberProperty();
+
+            foreach (var property in publicProperties)
+            {
+
+                codeMemberProperty.Name = property.Key;
+                codeMemberProperty.Type = new CodeTypeReference(property.Value);
+                codeMemberProperty.Attributes = MemberAttributes.Public;
+                codeMemberProperty.GetStatements.Add(new CodeMethodReturnStatement());
+                codeMemberProperty.Name += "{get; set;}//";
+
+                
+            }
+        }
+    }
+}
